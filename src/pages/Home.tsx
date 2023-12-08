d40fa9682fc6168ec69e4c9641f8ffe9e59a57b9
Felicia <felicia.lyden@gmail.com>
2023-12-08 12:16:12 +0100

    create Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,26 @@
+import { useContext } from "react"
 import IngredientsList from "../components/IngredientsList"
 import SearchRecipesForm from "../components/SearchRecipesForm"
+import { RecipeContext } from "../contexts/RecipeContext"
 
 const Home = () => {
+
+    const { searchValues } = useContext(RecipeContext)
+
+    const handleSearchRecipes = async() => {
+        const recipes = await fetch(`http://localhost:3001/api/recipes?ingredients=${searchValues.toString()}`);
+        const jsonRecipes = await recipes.json();
+        console.log("frontend response", jsonRecipes);
+    }
+
+
   return (
-    <>
-    <SearchRecipesForm></SearchRecipesForm>
-    <IngredientsList></IngredientsList>
-    </>
+    <div className="flex flex-col gap-2 p-7">
+    <h1 className="text-4xl text-center mb-5">What can I cook?</h1>
+    <SearchRecipesForm />
+    <IngredientsList />
+    <button onClick={handleSearchRecipes} className="btn btn-primary btn-sm mt-10">Search recipes</button>
+    </div>
   )
 }
 
