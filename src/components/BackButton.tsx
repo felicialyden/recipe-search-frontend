20a3ec8bc97e82833a7ed6ea8bff93de20833dad
Felicia <felicia.lyden@gmail.com>
2023-12-08 15:13:23 +0100

    add icon in back button

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -4,6 +4,20 @@ const BackButton = () => {
   const navigate = useNavigate();
   return (
     <button className="btn btn-sm btn-primary" onClick={() => navigate(-1)}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        className="w-4 h-4"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+        />
+      </svg>
       Back
     </button>
   );
