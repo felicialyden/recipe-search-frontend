3c809795b2e7fea024f5818e82e3bd523cb86c03
Felicia Lydén <felicia.lyden@gmail.com>
2024-01-18 13:36:27 +0100

    add info popup to recipe preview

--- src/components/RecipePreview.tsx
+++ src/components/RecipePreview.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import RecipeInfoPopup from "./RecipeInfoPopup";
 
 type RecipePreviewProps = {
   id: number;
@@ -19,12 +20,20 @@ const RecipePreview = ({
 
   return (
     <>
-      <li className="list-none flex mt-2 cursor-pointer" onClick={() => navigate(`/${id}`)}>
+      <li
+        className="list-none flex mt-2 cursor-pointer"
+        onClick={() => navigate(`/${id}`)}
+      >
         <img className="h-24 m-2" src={img} alt={title} />
         <div className="text-sm pt-3">
           <p className="font-semibold">{title}</p>
-          {usingIngredients && <p>Ingredients used: {usingIngredients}</p>}
-          {missingIngredients && <p>Ingredients missing: {missingIngredients}</p>}
+          <div className="flex gap-2">
+            <div>
+              {usingIngredients && <p>Ingredients used: {usingIngredients}</p>}
+              {missingIngredients && (<p>Ingredients missing: {missingIngredients}</p>)}
+            </div>
+            <RecipeInfoPopup />
+          </div>
         </div>
       </li>
       <div className="divider divider-secondary m-0"></div>
