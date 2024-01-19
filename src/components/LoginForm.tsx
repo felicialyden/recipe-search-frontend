f458885d43d07ac6132e6e5978a4c511dfc1c3bb
Felicia Lydén <felicia.lyden@gmail.com>
2024-01-19 07:51:34 +0100

    add login form component

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,14 +1,23 @@
-
 const LoginForm = () => {
   return (
-    <form>
-<input type="text" placeholder="Username" className="input input-bordered w-full max-w-xs" />
-<input type="email" placeholder="Email" className="input input-bordered w-full max-w-xs" />
-<button className="btn btn-primary">Log in</button>
-
-
+    <form className="flex flex-col gap-2 max-w-xs self-center w-full">
+      <h1>Log in</h1>
+      <input
+        type="text"
+        placeholder="Username"
+        className="input input-bordered w-full w-xs"
+        required
+      />
+      <input
+        type="email"
+        placeholder="Email"
+        className="input input-bordered w-full max-w-xs"
+        required
+      />
+      <button className="btn btn-primary max-w-xs">Log in</button>
+      <p className=" mt-4">Don't have an account yet? Sign up</p>
     </form>
-  )
-}
+  );
+};
 
-export default LoginForm
+export default LoginForm;
