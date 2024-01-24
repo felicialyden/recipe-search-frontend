03a643d6252074b9f60ccdcd972a598b07021a57
Felicia Lydén <felicia.lyden@gmail.com>
2024-01-24 09:40:07 +0100

    show ingredient list items inline

--- src/components/IngredientsList.tsx
+++ src/components/IngredientsList.tsx
@@ -13,9 +13,9 @@ const IngredientsList = ({ title, ingredients, removeFunction }: IngredientListP
     <ul className="mt-5">
       <h3>{`${title}: `}</h3>
       {ingredients.map((ingredient) => (
-        <li key={ingredient}>
+        <li key={ingredient} className="rounded-md bg-secondary px-2 py-1 m-0.5 inline-block">
           <div className="flex text-lg items-center gap-1" >
-            <p>{ingredient}</p>
+            <p className="text-base">{ingredient}</p>
             <button
               onClick={() => removeFunction(ingredient)}
               className="btn btn-square btn-ghost btn-xs"
@@ -23,7 +23,6 @@ const IngredientsList = ({ title, ingredients, removeFunction }: IngredientListP
              <X size={16}/>
             </button>
           </div>
-          <div className="divider divider-accent m-0" key={`divider${ingredient}`}></div>
         </li>
       ))}
     </ul>
